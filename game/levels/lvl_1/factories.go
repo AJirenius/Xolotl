d9d5ed1192316825e021db23083704789f71fe8b
Andreas Jirenius <ajirenius@gmail.com>
2016-08-27 10:06:49 +0200

    restored from weird build crash

---
+++ game/levels/lvl_1/factories.go
@@ -0,0 +1,34 @@
+embedded_components {
+  id: "arrow"
+  type: "factory"
+  data: "prototype: \"/game/arrow/arrow.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "arrow_wall"
+  type: "factory"
+  data: "prototype: \"/game/arrow/arrow_wall.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
